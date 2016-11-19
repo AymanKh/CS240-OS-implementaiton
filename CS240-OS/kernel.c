afb018901fc024717bec79dd56ef7eb3022b3a35
Iaman Alkhalaf <ialkhala@purdue.edu>
2016-11-20 01:20:47 +0300

    Merge remote-tracking branch 'origin/Ayman' into Ayman
    # Conflicts:
    #	CS240-OS/kernel.c

--- CS240-OS/kernel.c
+++ CS240-OS/kernel.c
@@ -4,45 +4,51 @@
 //
 //  Created by Mootaz N. Elnozahy on 11/12/16.
 //  Copyright © 2016 Mootaz N. Elnozahy. All rights reserved.
-//
+//  
 
 #include "kernel.h"
 #include "interrupts.h"
 
-
-
 void kernel_start()
 {
     set_debug_mode(DEBUG_ALL);
+    reset_debug_mode();
     start_console();
     initialize_memory();
     start_disk();
     
     
     char a[80];
-    sprintf(a, "Hello World %d \n",jiffies);
+    sprintf(a, "Hello World \n");
     write_console((unsigned) strlen(a), a);
     
-    // get the address of the clock interrupt
-    void (*clockInterruptPointer)(int) = &ClockInterrupt;
-    clockInterruptPointer(0);
-    
     // set the vector table
-    set_ivec(I_CLK, clockInterruptPointer);
+<<<<<<< HEAD
+    set_ivec(I_CLK, ClockInterruptHandler);
     
     char s[20];
     
     // print characters to screen, just to see if clock interrupt will actually interrupt the kernel (it should!)
-    for (int i = 0; i < 10000; i++)
-    {
-        sprintf(s,"%d ",i);
-        write_console((unsigned)strlen(s),s);
-    }
+//    for (int i = 0; i < 10000; i++)
+//    {
+//        sprintf(s,"%d ",i);
+//        write_console((unsigned)strlen(s),s);
+//    }
     
     // Examine current value of jiffies
     sprintf(s,"jiffies = %d \n",jiffies);
     write_console((unsigned)strlen(s), s);
     
     
+    
+    
+    
+    
     shutdown_machine();
+=======
+    SetAllHandlers();
+    
+    halt(); //This halt is just to test the clock interrupt handler
+    //shutdown_machine();
+>>>>>>> origin/Ayman
 }
